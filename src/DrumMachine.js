854bc997948b44554f2d6704b84840fb45c4fb28
Ryan Schafer <schaferyan@gmail.com>
2021-11-20 20:33:58 -0800

    Added local .wav files and linked to these rather than use absolute links to files on the web

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -6,47 +6,57 @@ const sounds = [
   {
     name: "ANALOG KICK",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/18[kb]analogbd.wav.mp3"
+    	"samples/Kicks/analogbd.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/18[kb]analogbd.wav.mp3"
+
   },
   {
     name: "ACCOUSTIC SNARE",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/61[kb]acoustic_snare.wav.mp3"
+    	"samples/Snares/acoustic_snare.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/61[kb]acoustic_snare.wav.mp3"
   },
   {
     name: "CLEAR HAT",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/14[kb]clear-hat.wav.mp3"
+    	"samples/Hats/clear-hat.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/14[kb]clear-hat.wav.mp3"
   },
   {
     name: "AMBIENT TOM",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/264[kb]ambient_tom_1.wav.mp3"
+    	"samples/Toms/ambient_tom_1.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/264[kb]ambient_tom_1.wav.mp3"
   },
   {
     name: "HAND CLAP",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Claps/7[kb]HandClap.wav.mp3"
+    	"samples/Claps/HandClap.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Claps/7[kb]HandClap.wav.mp3"
   },
   {
     name: "NORMAL CRASH",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Crashes/123[kb]normal-crash.wav.mp3"
+    	"samples/Crashes/normal-crash.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Crashes/123[kb]normal-crash.wav.mp3"
   },
   {
     name: "CRYSTAL RIDE",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Rides/139[kb]crystal_ride.wav.mp3"
+    	"samples/Rides/crystal_ride.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Rides/139[kb]crystal_ride.wav.mp3"
   },
   {
     name: "SATISFY 1",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Melodic%20Stabs%20and%20Hits/1166[kb]stab-satisfying-1.wav.mp3"
+    	"samples/Melodic_Hits/stab-satisfying-1.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Melodic%20Stabs%20and%20Hits/1166[kb]stab-satisfying-1.wav.mp3"
   },
   {
     name: "SATISFY 2",
     src:
-      "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Melodic%20Stabs%20and%20Hits/1166[kb]stab-satisfying-10.wav.mp3"
+    	"samples/Melodic_Hits/stab-satisfying-10.wav"
+      // "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Melodic%20Stabs%20and%20Hits/1166[kb]stab-satisfying-10.wav.mp3"
   }
 ];
 
