fa437d74006249a289e3415250a458616c420983
Ryan Schafer <schaferyan@gmail.com>
2021-12-04 21:23:36 -0800

    added recording and playback functionality and controls added metronome and bpm control configured pads to respond to mousedown instead of onclick(improves latency)

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './DrumMachine.css';
-import {playSample, getDuration, getSampleName, setGain} from './audioFunctions'
+import {playSample, getDuration, getSampleName, setGain, recordStart, stop, playLoop, setBpm, getInstant} from './audioFunctions'
 
 const Display = (props) => {
   return <div id="display">{props.display}</div>;
@@ -8,20 +8,32 @@ const Display = (props) => {
 
 const MasterVolume = (props) => {
   return (
-    <div className="control-element">
+    <div className="control-element" id="master-volume">
       <input type="range" className="slider" min="0" max="1" step="0.02"  onChange={props.handleChange} />
       <label className="slider-label">Volume</label>
     </div>
   );
 };
 
+// TODO hide curser, force double, change to 'type="number"' and add custom logic to only show numerial values within range, or '' for 0
+const TempoControl = (props) => {
+	return(
+		<div className="control-element" id="tempo-control" >
+			<input className="text-input" type="number" min="40.0" max="900.0" value={props.bpm} onChange={props.handleChange}/>
+			<label>bpm</label>
+		</div> 
+		)
+}
+
+
 const DrumPad = (props) => {
   const handleClick = (event) => {
+  	console.log(props.char + " clicked at " + getInstant() );
     props.onPadTriggered(props.char);
   };
 
   return (
-    <div className="drum-pad" id={props.char} onClick={handleClick}>
+    <div className="drum-pad" id={props.char} onMouseDown={handleClick}>
       {props.char}
     </div>
   );
@@ -31,10 +43,11 @@ const DrumPad = (props) => {
 const DrumPadArea = (props) => {
   return (
     <div id="drum-pad-area">
-      {props.keyset.map((char) => (
+      {props.keyset.map((char, index) => (
         <DrumPad
           char={char}
           onPadTriggered={props.onPadTriggered}
+          key={char + index}
         />
       ))}
     </div>
@@ -45,6 +58,12 @@ const ControlArea = (props) => {
   return (
     <div id="control-area">
       <MasterVolume handleChange={props.handleVolChange} />
+      
+      <button onClick={props.handleRecordStart} id="button-record" className="playback-control">REC</button>
+      <button onClick={props.handleRecordStop} id="button-stop" className="playback-control">STOP</button>
+      <button onClick={props.handlePlayPressed} id="button-play" className="playback-control">PLAY</button>
+
+      <TempoControl handleChange={props.handleBpmChange} bpm={props.bpm}/>
     </div>
   );
 };
@@ -52,14 +71,33 @@ const ControlArea = (props) => {
 export default class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { display: "PLAY ME", vol: 1.0 };
+    this.state = { display: "PLAY ME", vol: 1.0, bpm: 120.0 };
     this.setDisplay = this.setDisplay.bind(this);
     this.onPadTriggered = this.onPadTriggered.bind(this);
     this.handleVolChange = this.handleVolChange.bind(this);
+    this.handleRecordStart = this.handleRecordStart.bind(this);
+    this.handleRecordStop = this.handleRecordStop.bind(this);
+    this.handlePlayPressed = this.handlePlayPressed.bind(this);
+  }
+
+  handleRecordStart(){
+  	this.setState({recording: true});
+  	recordStart();
+  }
+
+  handleRecordStop(){
+  	this.setState({recording: false});
+  	stop();
+  }
+
+  handlePlayPressed(){
+  	this.setState({playing: true});
+  	playLoop();
   }
 
   onPadTriggered(char) {
-    playSample( char, 0, this.state.vol);
+  	console.log(char + " pad triggered at" + getInstant())
+    playSample( char, 0, this.state.recording);
     const name = getSampleName(char);
     this.setDisplay(name);
     const pad = document.getElementById(char);
@@ -80,9 +118,18 @@ export default class DrumMachine extends React.Component {
     setGain(this.state.vol);
   };
 
+  handleBpmChange = (event) => {
+  	this.setState({ bpm: event.target.value * 1.0 });
+  	if (this.state.bpm > 40.0){
+  		setBpm(this.state.bpm);
+  	}
+  }
+
   onKeyDown = (event) => {
-    const keyCode = event.which || event.keyCode;
-    const char = String.fromCharCode(keyCode);
+  	console.log("key pressed at " + getInstant())
+    const char = event.key.toUpperCase();
+    // const char = String.fromCharCode(keyCode);
+    console.log(char)
     if(this.props.keyset.includes(char)){
       this.onPadTriggered(char);
     }
@@ -98,11 +145,11 @@ export default class DrumMachine extends React.Component {
         id="drum-machine"
         onKeyDown={this.onKeyDown}
         tabIndex="0"
-        ref="component"
-      >
+        ref="component">
         <Display display={this.state.display} />
         <DrumPadArea keyset={this.props.keyset} onPadTriggered={this.onPadTriggered} />
-        <ControlArea handleVolChange={this.handleVolChange} />
+        <ControlArea bpm={this.state.bpm} handlePlayPressed={this.handlePlayPressed} handleVolChange={this.handleVolChange} 
+        handleRecordStart={this.handleRecordStart} handleRecordStop={this.handleRecordStop} handleBpmChange={this.handleBpmChange}/>
       </div>
     );
   }
--- src/audioFunctions.js
+++ src/audioFunctions.js
@@ -3,13 +3,25 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
 const soundBank = {};
+
 let gainNode;
 
-window.addEventListener("mousemove", function() {
+let startTime = null;
+let bpm = 120.0;
+let quantization = 0.5;
+const triggerList = [];
+
+let currentBeat = 0;
+let lookahead = 25.0;
+let scheduleAheadTime = 0.1;
+let nextClickTime = 0.0;
+let clickIntervalID = null;;
+
+document.addEventListener("mousemove", function() {
   audioContext.resume();
 }, {once : true});
 
-window.addEventListener("keydown", function() {
+document.addEventListener("keydown", function() {
   audioContext.resume();
 }, {once : true});
 
@@ -34,16 +46,59 @@ async function prepAudio( samples, keyset){
   gainNode.connect(audioContext.destination);
 }
 
+function recordStart(){
+  console.log("record request recieved");
+  countOff();
+  startMetronome();
+  playLoop();
+  console.log("record started at " + startTime)
+}
+
+function countOff(){
+  const countStart = audioContext.currentTime;
+  const beatLength = 60.0/bpm;
+  for(let i=0; i< 4; i++){
+    scheduleClick(0, countStart + i * beatLength);
+  };
+  startTime = countStart + 4 * beatLength;
+}
+
+function stop(){
+  console.log("triggerList =");
+  for(let i=0; i < triggerList.length; i++){
+    console.log(triggerList[i].char + " pressed at " + triggerList[i].time);
+  }
+  // stop the metronome
+  clearInterval(clickIntervalID);
+  startTime = null;
+}
+
 function getAudioBuffer(char){
   return soundBank[char].sample;
 }
 
-function playSample( char, time, vol) {
+function playSample( char, time, recording) {
+    const currentTime = audioContext.currentTime;
     const sampleSource = audioContext.createBufferSource();
     sampleSource.buffer = getAudioBuffer(char);
-    // gainNode.gain.value = vol;
     sampleSource.connect(gainNode);
     sampleSource.start(time);
+    console.log("recieved trigger event " + char + " at " + currentTime );
+    console.log("started sample at " + audioContext.currentTime);
+    if(recording){
+      recordTrigger(currentTime, char);
+    }
+}
+
+function recordTrigger(currentTime, char){
+  let triggerTime = currentTime - startTime;
+  if(triggerTime > 0){triggerList.push({char: char, time: quantize(triggerTime)})};
+}
+
+function quantize(time){
+  const beatLength = 60.0/bpm;
+  const beats = time/(beatLength * quantization);
+  return Math.round(beats);
 }
 
 function getDuration(char){
@@ -58,4 +113,62 @@ function setGain(vol){
   gainNode.gain.value = vol;
 }
 
-export{setupSamples, prepAudio, playSample, getDuration, getSampleName, setGain};
+function scheduleClick(beatNumber, time){
+    const osc = audioContext.createOscillator();
+    const envelope = audioContext.createGain();
+
+    osc.frequency.value = (beatNumber % 4 === 0) ? 1000 : 800;
+    envelope.gain.value = 1;
+    envelope.gain.exponentialRampToValueAtTime(1, time + 0.001);
+    envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
+    osc.connect(envelope);
+    envelope.connect(audioContext.destination); 
+    osc.start(time);
+    osc.stop(time + 0.03);
+}
+
+function nextBeat() {
+    console.log("scheduler advancing to next beat")
+    let secondsPerBeat = 60.0 / bpm;        
+    nextClickTime += secondsPerBeat;    // Add beat length to last beat time
+    currentBeat++;    // Advance the beat number, wrap to zero
+    if (currentBeat === 4) {
+        currentBeat = 0;
+    }
+}
+
+function scheduler(){
+  let loopExit = audioContext.currentTime + scheduleAheadTime
+  while (nextClickTime < loopExit ) {
+        scheduleClick( currentBeat, nextClickTime );
+        nextBeat();
+    }
+    
+}
+
+function startMetronome(){
+  currentBeat = 0;
+  nextClickTime = audioContext.currentTime;
+  clickIntervalID = setInterval(scheduler, lookahead);
+  console.log("starting metronome");
+}
+
+function playLoop(){
+  if(startTime === null){
+    startTime = audioContext.currentTime;
+  }
+  for(let i=0; i < triggerList.length; i++){
+    const time = startTime + (triggerList[i].time * 60.0/bpm * quantization);
+    playSample(triggerList[i].char, time , false);
+  }
+}
+
+function setBpm(newTempoInBpm){
+  bpm = newTempoInBpm;
+}
+
+function getInstant(){
+  return audioContext.currentTime;
+}
+
+export{setupSamples, prepAudio, playSample, getDuration, getSampleName, setGain, recordStart, stop, startMetronome, playLoop, setBpm, getInstant};
